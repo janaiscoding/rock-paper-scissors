4f4a792d1ac94603f6430150436032288c8963ad
JanaIsCoding <jana.istrate@gmail.com>
2022-11-11 23:32:14 +0200

    Finish bo5 RPS game

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
-// what we use is
-let computerSelection;
-let playerSelection;
+// what we use is:
+
 let computerScore = 0;
 let playerScore = 0;
 const buttons = document.querySelectorAll(".button");
-const comp = document.querySelector(".computer-score");
+const para = document.querySelector('.winner-text');
+const infoPlayer = document.querySelector('.player-selection');
+const infoComputer = document.querySelector('.computer-selection');
 
 // randomize and select AI choice
   function getComputerChoice(){
@@ -12,45 +13,74 @@ const comp = document.querySelector(".computer-score");
   let computerInput = choices[Math.floor(Math.random()*choices.length)]; 
   return computerInput;
 } 
-
+// player choices --- painchamp.
 buttons.forEach((button) => {
   button.addEventListener('click',()=>{
     playerSelection = button.id;
     computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-
-    if ( playerScore === 5 || computerScore === 5) {
-      finalWinner();
-    }
-  })
-})
+    playGame();
+    infoPlayer.textContent = `You played ${playerSelection}`;
+    infoComputer.textContent = `Computer played ${computerSelection}`;
+  });
+});
 
 // on each press, get player selection and computer selection
 function playRound(playerSelection, computerSelection) {
-  const para = document.querySelector('.winner-text');
-  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    // tie situation
+  if (playerSelection == computerSelection) {
     para.textContent = "It's a tie";
-    } // tie situation 
-
-  else if  ( 
-      (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper" ) || 
-      (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors" ) ||
-      (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock" ) 
-      ) {
-        para.textContent = "You lost";
-      computerScore = computerScore++;
-  } // loss situation 
+    }  
+    // loss situation 
+  else if( 
+      (playerSelection == "rock" && computerSelection == "paper" ) || 
+      (playerSelection == "paper" && computerSelection == "scissors" ) ||
+      (playerSelection == "scissors" && computerSelection == "rock" ) 
+      ) {     
+      para.textContent = "You lost";
+      computerScore++;
+      keepComputerScore();
+  } 
+    // win situation
   else if ( 
-      (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors" ) ||
-      (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock" ) ||
-      (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper" )
+      (playerSelection == "rock" && computerSelection == "scissors" ) ||
+      (playerSelection == "paper" && computerSelection == "rock" ) ||
+      (playerSelection == "scissors" && computerSelection == "paper" )
   ){
-    para.textContent = "You won";
-      playerScore = ++playerScore;
-  } // win situation
+      para.textContent = "You won";
+      playerScore++;
+      keepPlayerScore();   
+  } 
+  
  }
- function finalWinner(){
-    if (computerScore === 5){
 
-    }
+ //full game on each click, play round til bo5 is decided.
+function playGame(){
+  playRound(playerSelection, computerSelection)
+  if(playerScore == 5){
+    para.textContent = `You won, final score ${playerScore} - ${computerScore}`;
+    resetGame();
+  }
+  else if (computerScore == 5 ) {
+    para.textContent = `Computer won, final score: ${computerScore} - ${playerScore}`;
+    resetGame();
+  }
 }
+// never ending best of 5 RPS game, no questions asked.
+function resetGame(){
+  playerScore = 0; 
+  computerScore = 0; 
+  keepPlayerScore();
+  keepComputerScore();
+}
+
+
+function keepPlayerScore() {
+  const playerCounter = document.querySelector(".player-score");
+  playerCounter.textContent = playerScore;
+}
+
+function keepComputerScore() {
+  const computerCounter = document.querySelector(".computer-score");
+  computerCounter.textContent = computerScore;
+}
+
